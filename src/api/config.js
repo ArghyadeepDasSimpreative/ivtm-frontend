dd8e53cc0da464a18bb81e21280a280bdeb7d33f
Pushpam Singh <pushpam.singh@optinovo.com>
2025-11-14 12:15:51 +0530

    comment updated

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+//comment
 // const BASE_URL = import.meta.env.VITE_APP_MODE !== "deployment" ? import.meta.env.VITE_API_BASE_URL : 'http://localhost:5400'
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5400';
 
